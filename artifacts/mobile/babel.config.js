76a0a6cd4a2aeab4eb4b42deba6c74b98d6d49bc
Prakasharyal210 <60083649-prakasharyal210@users.noreply.replit.com>
2026-06-08 08:30:15 +0000

    Transform react-native/src/private classes via babel overrides

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -2,5 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
+    overrides: [
+      {
+        test: /react-native[\\/]src[\\/]private/,
+        plugins: [
+          ['@babel/plugin-transform-classes', { loose: true }],
+          ['@babel/plugin-transform-class-properties', { loose: true }],
+          ['@babel/plugin-transform-private-methods', { loose: true }],
+          ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+        ],
+      },
+    ],
   };
 };
