bce7b994a13ca1ff417f1ef4e3fea484b5261db2
Prakasharyal210 <60083649-prakasharyal210@users.noreply.replit.com>
2026-06-08 03:09:29 +0000

    Target react-native, worklets and tanstack for private fields

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -4,7 +4,11 @@ module.exports = function (api) {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     overrides: [
       {
-        test: /node_modules\/.pnpm\/react-native@.*\/node_modules\/react-native\/.*\.js$/,
+        test: [
+          /node_modules\/.pnpm\/react-native@.*\/node_modules\/react-native\/.*\.js$/,
+          /node_modules\/.pnpm\/react-native-worklets@.*\.js$/,
+          /node_modules\/.pnpm\/@tanstack\/.*\.js$/,
+        ],
         plugins: [
           ["@babel/plugin-transform-class-properties", { loose: true }],
           ["@babel/plugin-transform-private-methods", { loose: true }],
