eb73b9d5cf54edfe2c1d05dfd9eb8a89bb057fd8
Prakasharyal210 <60083649-prakasharyal210@users.noreply.replit.com>
2026-06-08 02:57:23 +0000

    Fix DOMRectReadOnly private fields only

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -2,5 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    overrides: [
+      {
+        test: /node_modules.*react-native.*DOMRectReadOnly\.js$/,
+        plugins: [
+          ["@babel/plugin-transform-class-properties", { loose: true }],
+          ["@babel/plugin-transform-private-methods", { loose: true }],
+        ],
+      },
+    ],
   };
 };
