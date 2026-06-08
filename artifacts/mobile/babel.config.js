4635fcf501efe90c27fea3bf3299edd20b04b9b5
Prakasharyal210 <60083649-prakasharyal210@users.noreply.replit.com>
2026-06-08 07:32:43 +0000

    Fix babel plugin names: proposal -> transform

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -6,9 +6,9 @@ module.exports = function (api) {
       {
         test: /node_modules/,
         plugins: [
-          ['@babel/plugin-proposal-class-properties', { loose: true }],
-          ['@babel/plugin-proposal-private-methods', { loose: true }],
-          ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+          ['@babel/plugin-transform-class-properties', { loose: true }],
+          ['@babel/plugin-transform-private-methods', { loose: true }],
+          ['@babel/plugin-transform-private-property-in-object', { loose: true }],
         ],
       },
     ],
