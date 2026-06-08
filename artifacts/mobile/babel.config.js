bc7c082e8e35fcf920708b2e64255cb9cf8e2b58
Prakasharyal210 <60083649-prakasharyal210@users.noreply.replit.com>
2026-06-08 07:22:41 +0000

    Use babel overrides for node_modules to transform private fields

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -2,5 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
+    overrides: [
+      {
+        test: /node_modules/,
+        plugins: [
+          ['@babel/plugin-proposal-class-properties', { loose: true }],
+          ['@babel/plugin-proposal-private-methods', { loose: true }],
+          ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+        ],
+      },
+    ],
   };
 };
