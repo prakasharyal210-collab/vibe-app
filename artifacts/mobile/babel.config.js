13a0130ce8adb0317e6cf528ccdb4c0ca93e7aca
Prakasharyal210 <60083649-prakasharyal210@users.noreply.replit.com>
2026-06-08 08:42:00 +0000

    Broaden babel override to all react-native/src files

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -4,13 +4,14 @@ module.exports = function (api) {
     presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
     overrides: [
       {
-        test: /react-native[\\/]src[\\/]private/,
+        test: /node_modules[\\/]react-native[\\/]src[\\/]/,
         plugins: [
           require('@babel/plugin-transform-flow-strip-types'),
           ['@babel/plugin-transform-classes', { loose: true }],
           ['@babel/plugin-transform-class-properties', { loose: true }],
           ['@babel/plugin-transform-private-methods', { loose: true }],
           ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+          ['@babel/plugin-transform-private-fields', { loose: true }],
         ],
       },
     ],
